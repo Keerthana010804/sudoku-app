9a7da55cecf7c63cb441e18c59d2fe1f471db875
Keerthana <keerthana010804@gmail.com>
2026-03-14 18:15:36 +0530

    Fix hint system to return correct values using solution board

--- lib/features/sudoku/viewmodel/sudoku_viewmodel.dart
+++ lib/features/sudoku/viewmodel/sudoku_viewmodel.dart
@@ -19,6 +19,18 @@ class SudokuViewModel extends ChangeNotifier {
     [0, 0, 0, 0, 8, 0, 0, 7, 9],
   ];
 
+  List<List<int>> solution = [
+    [5,3,4,6,7,8,9,1,2],
+    [6,7,2,1,9,5,3,4,8],
+    [1,9,8,3,4,2,5,6,7],
+    [8,5,9,7,6,1,4,2,3],
+    [4,2,6,8,5,3,7,9,1],
+    [7,1,3,9,2,4,8,5,6],
+    [9,6,1,5,3,7,2,8,4],
+    [2,8,7,4,1,9,6,3,5],
+    [3,4,5,2,8,6,1,7,9],
+  ];
+
   List<List<bool>> isFixedCell = [
     [true, true, false, false, true, false, false, false, false],
     [true, false, false, true, true, true, false, false, false],
@@ -118,14 +130,8 @@ class SudokuViewModel extends ChangeNotifier {
     if (selectedRow == -1 || selectedCol == -1) return;
 
     if (board[selectedRow][selectedCol] != 0) return;
-    List<List<int>> tempBoard = board
-        .map((row) => List<int>.from(row))
-        .toList();
-    if (solveSudoku(tempBoard)) {
-      int correctNumber = tempBoard[selectedRow][selectedCol];
-      board[selectedRow][selectedCol] = correctNumber;
-      notifyListeners();
-    }
+
+    board[selectedRow][selectedCol] = solution[selectedRow][selectedCol];
   }
 
   void togglePencil() {
